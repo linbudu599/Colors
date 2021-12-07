f6858ef8bdffd8f4c13b181da8ef10a149147c79
linbudu599 <linbudu599@gmail.com>
2021-12-07 22:37:53 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { GeistProvider, CssBaseline } from "@geist-ui/react";
+import { Button } from "@geist-ui/react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <GeistProvider>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hello Vite + React!</p>
+          <Button>Click Me</Button>
+          <p>
+            <button
+              type="button"
+              onClick={() => setCount((count) => count + 1)}
+            >
+              count is: {count}
+            </button>
+          </p>
+          <p>
+            Edit <code>App.tsx</code> and save to test HMR updates.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            {" | "}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Vite Docs
+            </a>
+          </p>
+        </header>
+      </div>
+    </GeistProvider>
+  );
+}
+
+export default App;
